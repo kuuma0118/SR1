99b5fadaa17139211bce6fc5c300afd24fdfbb8c
渡邉空馬 <k022g0093@g.neec.ac.jp>
2024-12-23 17:53:42 +0900

    0902

---
+++ Item/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ public class Player
+{
+    // プライベートフィールド
+    private Item item;
+
+    // コンストラクタ
+    public Player(Item item)
+    {
+        this.item = item;
+    }
+
+    // メソッド
+    public void UseItem()
+    {
+        if (item != null)
+        {
+            item.Use();
+        }
+        else
+        {
+            Console.WriteLine("アイテムを持っていません。");
+        }
+    }
+}
+
--- Item/Program.cs
+++ Item/Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 
 namespace pj09_1
-{
+{ 
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Itemクラスのインスタンスを作成
-            Item item = new Item("つるはし");
+            // Playerクラスのインスタンスを生成し、Itemクラスのインスタンスを渡す
+            Player player = new Player(new Item("こん棒"));
 
-            // Useメソッドを呼び出し
-            item.Use();
+            // UseItemメソッドを呼び出し
+            player.UseItem();
 
+            // コンソールがすぐ閉じないようにする
             Console.ReadLine();
         }
     }
