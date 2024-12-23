044667f4608c0f006b052b3b4240abff44503a8c
渡邉空馬 <k022g0093@g.neec.ac.jp>
2024-12-23 17:46:40 +0900

    0901

---
+++ Item/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ public class Item
+{
+    // プライベートフィールド
+    private string name;
+
+    // コンストラクタ
+    public Item(string name)
+    {
+        this.name = name;
+    }
+
+    // メソッド
+    public void Use()
+    {
+        Console.WriteLine($"{name} を使用しました。");
+    }
+}
+
---
+++ Item/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pj09_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Itemクラスのインスタンスを作成
+            Item item = new Item("つるはし");
+
+            // Useメソッドを呼び出し
+            item.Use();
+
+            Console.ReadLine();
+        }
+    }
+
+}
